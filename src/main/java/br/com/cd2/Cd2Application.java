48a9eb59ff539f5d559105402f4110273fd846e5
mateusalves01 <axlmxx@gmail.com>
2022-09-01 20:44:20 -0300

    Inicializando projeto

---
+++ src/main/java/br/com/cd2/Cd2Application.java
@@ -0,0 +1,13 @@
+package br.com.cd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cd2Application.class, args);
+	}
+
+}
