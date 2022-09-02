5cf3e7ecbee56dcc89d8181644f51ad61a9eb54a
mateusalves01 <axlmxx@gmail.com>
2022-09-01 23:40:10 -0300

    Adicionado regra de negocios

--- src/main/java/br/com/cd2/controller/FreteController.java
+++ src/main/java/br/com/cd2/controller/FreteController.java
@@ -6,6 +6,7 @@ import br.com.cd2.service.FreteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class FreteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public FreteResponse calcularFrete(FreteRequest freteRequest){
+    public FreteResponse calcularFrete(@RequestBody FreteRequest freteRequest){
        return freteService.calcularFrete(freteRequest);
     }
 
--- src/main/java/br/com/cd2/service/impl/FreteServiceImpl.java
+++ src/main/java/br/com/cd2/service/impl/FreteServiceImpl.java
@@ -1,23 +1,66 @@
 package br.com.cd2.service.impl;
 
+import br.com.cd2.client.EnderecoClient;
 import br.com.cd2.request.FreteRequest;
+import br.com.cd2.response.EnderecoResponse;
 import br.com.cd2.response.FreteResponse;
 import br.com.cd2.service.FreteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @Service
+@RequiredArgsConstructor
 public class FreteServiceImpl implements FreteService {
+
+    public static final BigDecimal DESCONTO_CIQUENTA = BigDecimal.valueOf(0.5);
+    public static final BigDecimal DESCONTO_SETENTA_CINCO = BigDecimal.valueOf(0.75);
+    public static final BigDecimal PRECO_KG = BigDecimal.valueOf(1);
+    private final EnderecoClient enderecoClient;
+
 
     @Override
     public FreteResponse calcularFrete(FreteRequest freteRequest) {
-        //TODO buscar endereço
-
+        final EnderecoResponse enderecoOrigem = enderecoClient.getEndereco(freteRequest.getCepOrigem());
+        final EnderecoResponse enderecoDestino = enderecoClient.getEndereco(freteRequest.getCepDestino());
         //TODO Calcular Frete
+        // CEPs com DDDs iguais tem 50% de desconto no valor do frete e entrega prevista de 1 dia
+        FreteResponse freteResponse = new FreteResponse();
+        freteResponse.setCepDestino(freteRequest.getCepDestino());
+        freteResponse.setCepOrigem(freteRequest.getCepOrigem());
+        BigDecimal valorTotalFrete = BigDecimal.ZERO;
+        BigDecimal valorTotalFreteDesconto = BigDecimal.ZERO;
+
+        if (enderecoOrigem.getDdd().equals(enderecoDestino.getDdd())
+                && enderecoOrigem.getLocalidade().equals(enderecoDestino.getLocalidade())) {
+            valorTotalFrete = freteRequest.getPeso().multiply(PRECO_KG);
+            BigDecimal valorDesconto = valorTotalFrete.multiply(DESCONTO_SETENTA_CINCO);
+            valorTotalFreteDesconto = valorTotalFrete.subtract(valorDesconto);
+            freteResponse.setVlTotalFrete(valorTotalFreteDesconto);
+            freteResponse.setDataPrevistaEntrega(LocalDate.now().plusDays(3));
+        }
+
+        if (enderecoOrigem.getUf().equals(enderecoDestino.getUf())
+                && !enderecoOrigem.getLocalidade().equals(enderecoDestino.getLocalidade())) {
+            valorTotalFrete = freteRequest.getPeso().multiply(PRECO_KG);
+            BigDecimal valorDesconto = valorTotalFrete.multiply(DESCONTO_CIQUENTA);
+            valorTotalFreteDesconto = valorTotalFrete.subtract(valorDesconto);
+            freteResponse.setVlTotalFrete(valorTotalFreteDesconto);
+            freteResponse.setDataPrevistaEntrega(LocalDate.now().plusDays(1));
+        }
+
+        if (!enderecoOrigem.getUf().equals(enderecoDestino.getUf())) {
+            valorTotalFrete = freteRequest.getPeso().multiply(PRECO_KG);
+            freteResponse.setVlTotalFrete(valorTotalFrete);
+            freteResponse.setDataPrevistaEntrega(LocalDate.now().plusDays(10));
+        }
 
         //TODO Gravar no banco
 
         //TODO Retornar frete calculado
-        return null;
+        return freteResponse;
     }
 
 }
